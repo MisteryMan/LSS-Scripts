40bc9f1b0985b1bc9bf9291939b44259c101d2db
MisteryMan <MisteryMan@users.noreply.github.com>
2020-07-14 16:39:50 +0200

    Update layout01mod.user.js

--- layout01mod.user.js
+++ layout01mod.user.js
@@ -5,6 +5,7 @@
 // @description  Kleine aanpassingen van LSS-Manager.
 // @author       Alex Calsbeek
 // @match        https://www.meldkamerspel.com/
+// @match        https://meldkamerspel.com/
 // @match        https://www.missionchief.com/
 // @match        https://missionchief.com/
 // @grant        none
@@ -20,12 +21,17 @@
     document.getElementById("chat_outer").style.overflow = "hidden";
     document.getElementById("radio_outer").style.overflow = "hidden";
 
-$("head").append("<style type='text/css'>#map_outer { width: 40%; }" +
+$("head").append("<style type='text/css'>#map_outer { width: 40%; top: 0px!important; height: calc(101vh - 90px)!important; }" +
+                 "#missions-panel-body { height: 92%!important }" +
                  "#buildings_outer, #chat_outer, #missions_outer, #radio_outer { width: calc(60% - 110px); }" +
                  "div#mission_list { display: flex; flex-direction: column-reverse; }" +
-                 "div#missions_outer { height: calc( 53vh - 70px ); }" +
-                 "div#chat_outer { display: block; height: calc(42vh-0px); }" +
+                 "#map {     height: calc(100vh - 90px)!important; }" +
+                 ".alliance_true.btn-group { height: 23px; }" +
                  "</style>");
+    /*
+                     "div#missions_outer { height: calc( 53vh - 70px ); }" +
+                 "div#chat_outer { display: block; height: calc(46vh - 0px); }" +
+    */
 
 })();
 
